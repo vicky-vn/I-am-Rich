82c62cfc5f2dac699d827360a67666f394af1a23
Vignesh Natarajan <vikky33.nv@gmail.com>
2025-01-06 22:57:28 -0500

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Vignesh Natarajan on 12/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
